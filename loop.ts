c8f53156f4922b5c9db3f26f82d1029652df5347
minakshimore123 <113169498+minakshimore123@users.noreply.github.com>
2022-09-16 12:55:48 +0530

    Add files via upload

---
+++ loop.ts
@@ -0,0 +1,25 @@
+//while loop
+var a:number=1;   
+
+while(a<= 10)
+{
+    console.log(a);
+    a++;
+}
+
+//Do-while loop (statement will execute once after check condition)
+
+var b:number= 1;
+
+do{
+    console.log(b);
+    b++;
+}
+while(b<=10);
+
+//for loop
+
+for(var i:number=1;i<=10;i++)
+{
+    console.log(i);
+}
